0a51bec7052cf223b922ddb790fdd2560c31009b
claudietto <claudietto@users.noreply.github.com>
2026-07-07 15:51:35 +0200

    tmp3

--- typescript/mass-testing-report/src/globals.d.ts
+++ typescript/mass-testing-report/src/globals.d.ts
@@ -2,6 +2,8 @@
  * This file is distributed under the MIT License. See LICENSE.md for details.
  */
 
-// Side-effect CSS imports are handled by webpack's style-loader/css-loader; this
-// declaration just tells TypeScript such imports are valid.
+// Side-effect style imports are handled by webpack's style-loader/css-loader
+// (and sass-loader for .scss); these declarations just tell TypeScript such
+// imports are valid.
 declare module "*.css";
+declare module "*.scss";
--- typescript/mass-testing-report/src/main.tsx
+++ typescript/mass-testing-report/src/main.tsx
@@ -2,7 +2,7 @@
  * This file is distributed under the MIT License. See LICENSE.md for details.
  */
 
-import "./main.css";
+import "./main.scss";
 
 import { Database } from "@sqlite.org/sqlite-wasm";
 import { render } from "preact";
